61159f9cab50c1843950c8186752c29613f6510f
Ruslan Senatorov <55090151+Senatorov1@users.noreply.github.com>
2020-12-30 23:44:30 +0300

    Create random_happy.py

---
+++ random_happy.py
@@ -0,0 +1,18 @@
+# random happy
+
+import random
+
+random_happy = random.randint(1, 5)
+
+if random_happy == 1:
+    print("ВСЁ ПОЛУЧИТСЯ")
+elif random_happy == 2:
+    print("Я МОГУ ВСЁ")
+elif random_happy == 3:
+    print("ВСЁ В ТВОЕЙ ГОЛОВЕ")
+elif random_happy == 4:
+    print("УДИВИ ВСЕХ")
+elif random_happy == 5:
+    print("ТЫ СМОЖЕШЬ")
+else:
+    print("SENATOROV")
